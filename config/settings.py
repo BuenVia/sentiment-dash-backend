6c2742316822d05e69d6d0de44a6432e2fdc0674
Matthew Clifford <matthewclifford@hotmail.co.uk>
2025-08-27 17:28:21 +0200

    changed db settings in settings.py

--- config/settings.py
+++ config/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "feedback_db",
-        "USER": "feedback_user",
-        "PASSWORD": "feedback_pass",
-        "HOST": "127.0.0.1",
-        "PORT": "5433",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "feedback_db",
+#         "USER": "feedback_user",
+#         "PASSWORD": "feedback_pass",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5433",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
