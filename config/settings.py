5411b3d602abab404d6499a2e8050660d6b3d845
buenvia <matthewclifford@hotmail.co.uk>
2025-11-05 19:30:50 +0100

    updated cors settings

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = ["https://sentiment-dashboard.netlify.app/"]
+CORS_ALLOWED_ORIGINS = ["https://sentiment-dashboard.netlify.app/"]
